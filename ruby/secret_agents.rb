64c291a0bbfca8176f16dedf6996ebcffeaf06aa
Brent Unkrich <bunkrich@gmail.com>
2017-02-16 15:22:54 -0500

    moved all of the encrypt back to the top and deleted the line with the whole alphabet as it is repetatitve

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -23,6 +23,33 @@ end
 cipher_word = encrypt(message)
 =end
 
+puts "what do you want to encrypt?"
+message = gets.chomp
+
+def encrypt(message)
+
+	message_length = message.length
+
+	current_index = 0
+
+	message_number = 0
+
+	cipher_number = 0
+	
+	cipher_word = ""
+
+
+	until message_length == current_index 
+		message_letter = message[current_index]
+		message_number = "abcdefghijklmnopqrstuvwxyza".index(message_letter)
+		cipher_number = message_number + 1
+		cipher_letter = "abcdefghijklmnopqrstuvwxyza"[cipher_number]
+		current_index += 1
+		cipher_word = cipher_word + cipher_letter
+	end 
+puts cipher_word
+end
+encrypt(message)
 
 puts "what do you want to decrypt?"
 cipher_word1 = gets.chomp
@@ -39,7 +66,6 @@ def decrypt(cipher_word1)
 
 	cipher_number = 0
 
-	"abcdefghijklmnopqrstuvwxyz".index("solution_letter")
 
 	until cipher_length == current_index 
 		cipher_letter = cipher_word1[current_index]
@@ -55,35 +81,3 @@ puts solution_word
 end
 
 decrypt(cipher_word1)
-
-
-
-puts "what do you want to encrypt?"
-message = gets.chomp
-
-
-def encrypt(message)
-
-	message_length = message.length
-
-	current_index = 0
-
-	message_number = 0
-
-	cipher_number = 0
-	
-	cipher_word = ""
-
-	"abcdefghijklmnopqrstuvwxyza".index("cipher_letter")
-
-	until message_length == current_index 
-		message_letter = message[current_index]
-		message_number = "abcdefghijklmnopqrstuvwxyza".index(message_letter)
-		cipher_number = message_number + 1
-		cipher_letter = "abcdefghijklmnopqrstuvwxyza"[cipher_number]
-		current_index += 1
-		cipher_word = cipher_word + cipher_letter
-	end 
-puts cipher_word
-end
-encrypt(message)
