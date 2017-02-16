6bd092f2d4cb4d7617176464f0a1de9f198b5a70
Brent Unkrich <bunkrich@gmail.com>
2017-02-16 15:50:30 -0500

    made all the variable different in both defs

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -24,34 +24,35 @@ cipher_word = encrypt(message)
 
 
 # ENCRYPT part of the code
-puts "What do you want to encrypt?"
-message = gets.chomp
+#puts "What do you want to encrypt?"
+#message = gets.chomp
+
 
 def encrypt(message)
 	message_length = message.length
-	current_index = 0
+	current_index1 = 0
 	cipher_word = ""
 	message_number = 0
 	cipher_number = 0
 	
-	until message_length == current_index 
-		message_letter = message[current_index]
+	until message_length == current_index1
+		message_letter = message[current_index1]
 		message_number = "abcdefghijklmnopqrstuvwxyza".index(message_letter)
-		cipher_number = message_number + 1
-		cipher_letter = "abcdefghijklmnopqrstuvwxyza"[cipher_number]
-		current_index += 1
-		cipher_word = cipher_word + cipher_letter
+		cipher_number1 = message_number + 1
+		cipher_letter1 = "abcdefghijklmnopqrstuvwxyza"[cipher_number1]
+		current_index1 += 1
+		cipher_word = cipher_word + cipher_letter1
 	end 
 
 puts cipher_word
 end
 
-encrypt(message)
+# encrypt(message)
 
 
 # DECRYPT part of the code
-puts "what do you want to decrypt?"
-cipher_word1 = gets.chomp
+#puts "what do you want to decrypt?"
+#cipher_word1 = gets.chomp
 
 def decrypt(cipher_word1)
 	cipher_length = cipher_word1.length
@@ -72,4 +73,9 @@ def decrypt(cipher_word1)
 puts solution_word
 end
 
-decrypt(cipher_word1)
+# decrypt(cipher_word1)
+
+encrypt("swordfish")
+new_word = encrypt("swordfish")
+puts new_word
+decrypt("boys")
