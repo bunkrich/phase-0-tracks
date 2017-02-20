e17be518f81d9de023d5d1fd8b85a55540527ea7
Brent Unkrich <bunkrich@gmail.com>
2017-02-20 09:46:33 -0500

    did realease 1, added in the questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+	employee_name = gets.chomp
+puts "How old are you?"
+	employee_age = gets.chomp
+puts "What year were you born?"
+	employee_year = gets.chomp	
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
