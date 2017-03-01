98ba992baa0b26bfa1961667101a93cc542d4804
Brent Unkrich <bunkrich@gmail.com>
2017-03-01 01:10:37 -0500

    cleaned up the formating and added PUTS calls at the end

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,4 +1,4 @@
-#Restaurant
+#Restaurant Menu split into DRINKS then FOOD
 
 menu = {
 	drinks: {
@@ -14,7 +14,7 @@ menu = {
 			well_drink: 9,
 			cosmo: 12,
 			sazerac: 14
-			},
+		},
 		draft_beers: {
 		  ale: 7,
 			lager: 6,
@@ -22,12 +22,12 @@ menu = {
 			pale_ale: 8,
 			wheat_bear: 8,
 			porter: 7
-			},
-		bottle_beers: {
-			pilsner: 5,
-			cider: 6,
-			ale: 5,
-			},
+		},
+		bottle_beers: [
+			'pilsner',
+			'cider',
+			'ale'
+		], #beer prices are always the same 
 		wines: {
 			riesling: 9,
 			chardonnay: 12,
@@ -39,26 +39,37 @@ menu = {
 			champagne: 19,
 			cuve: 10
 		}
-		},
+	},
 	food: {
 		snacks: {
 			shrimp_cocktail: 12,
 			deviled_eggs: 7,
 			fries: 7,
 			cheese_plate: 15
-			},
+		},
 		pizzas: {
 			cheese: 18,
 			pepperoni: 20,
 			mushroom: 19,
 			veggie: 22
-			},
-		salads: {
-			iceberg: 11,
-			waldorf: 12,
-			caesar: 13,
-			garden: 8	
-			}
+		},
+		salads: [
+			'iceberg',
+			'waldorf',
+			'caesar',
+			'garden'	
+		] #salads are all the same price 
 	}
 }
-	puts menu[:food][:pizzas].1
+# puts out all of the pizzas and their prices
+	puts menu[:food][:pizzas]
+#puts out all the wine and their prices
+	puts menu[:drinks][:wines]
+#puts out the name of the first kind of bottle of beer 
+	puts menu[:drinks][:bottle_beers][0]
+#puts out the price of the cuve wine 
+	puts menu[:drinks][:wines][:cuve]
+#puts out a reverse order of salads	
+	puts menu[:food][:salads].reverse
+#puts the first salad added to the menu
+	puts menu[:food][:salads].first
