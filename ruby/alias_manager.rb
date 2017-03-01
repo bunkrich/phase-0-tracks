2f04bef3bab27da28ceaf8c9d11d1fff4632c064
Brent Unkrich <bunkrich@gmail.com>
2017-03-01 01:34:54 -0500

    added psuedo code and got the user input

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+#get name from user
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+reverse_name = "#{last_name} #{first_name}"
+p reverse_name
+#swap the names by using new variable to hold the full name
+
+#break up the full name into each letter
+
+#if a letter is a vowl change it to the next vowl
+
+#if a latter is a consonant change it to the next 
+
+#convert the name back to the spy
