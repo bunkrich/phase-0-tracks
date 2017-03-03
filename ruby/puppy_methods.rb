56e4fb2ca974b0cb5ea1b877e9aa86f0acc18bde
Brent Unkrich <bunkrich@gmail.com>
2017-03-03 13:30:06 -0500

    copied the code from my pairing as we switch drivers

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,41 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times do |talk|
+      talk = "woof"
+      puts talk
+    end
+  end
+
+  def roll_over
+    puts "rolls over"
+  end
+
+  def dog_years(h_years)
+    d_years = 7 * h_years
+    puts d_years
+  end
+
+  def commands(action)
+    puts "The dog will #{action}"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
+
 end
 
+labrador = Puppy.new
+
+p labrador.fetch("ball")
+
+p labrador.speak(5)
+
+p labrador.roll_over
+
+p labrador.dog_years(3)
+
+p labrador.commands("sit")
