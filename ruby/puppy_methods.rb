7542fcf7aca55fd7b22a9080f6b5cfaca84c07d3
Brent Unkrich <bunkrich@gmail.com>
2017-03-03 13:43:48 -0500

    added the Kitty class with 3 different methods and then did a loop to fill an array with 50 kitties

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,6 +32,21 @@ class Puppy
 
 end
 
+class Kitty
+
+	def initialize
+		puts "Welcome the new kitten to the family"
+	end
+
+	def sleep(hours)
+		puts "the kitten slept for #{hours} hours"
+	end
+
+	def purr(type_of_purr)
+		puts "the kitten is purring #{type_of_purr}"
+	end
+end
+
 labrador = Puppy.new
 
 p labrador.fetch("ball")
@@ -43,3 +58,17 @@ p labrador.roll_over
 p labrador.dog_years(3)
 
 p labrador.commands("sit")
+
+fluffy = Kitty.new
+
+p fluffy.sleep(4)
+
+p fluffy.purr("softly")
+
+Cat_family = []
+
+50.times do 
+	kitty = Kitty.new
+	Cat_family << kitty 
+end
+p Cat_family
