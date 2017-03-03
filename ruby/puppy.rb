373be7cb076b8a772728579d07e6140a2168cf50
Brent Unkrich <bunkrich@gmail.com>
2017-03-03 14:06:56 -0500

    added my comments about the dog characteristics and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#---Dog Type
+#pug, beagle, german shepherd, rottweiler, bulldog, etc
+
+#---Traits
+#Name
+#Color
+#Weight
+#Good with kids
+#Friendlyness
+#Protective
+
+#---Behavior
+#Speed
+#Snuggle
+#Bark
+#Playful
