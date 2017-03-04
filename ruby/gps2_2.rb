8cd1f5ce10a2987e6dbfed84e09e8df12560a613
Brent Unkrich <bunkrich@gmail.com>
2017-03-04 11:42:01 -0500

    added the final def to print out the list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -16,24 +16,47 @@ grocery_list = {
 # input: list, item name, and optional quantity
 # steps: define method, add item and quanity to specific list
 # output: the new list
+
 def add_item(list,new_item,item_count)
 	list[new_item] = item_count
 end
 
-p add_item(grocery_list,"grape",4)
-p grocery_list
+
 
 # Method to remove an item from the list
 # input: get the hash/list name and the key to delete
 # steps: then delete the key from the hash
 # output: updated list, minus what we deleted
 
+def delete_item(list,item)
+	list.delete(item)
+end
+
 # Method to update the quantity of an item
 # input: get the hash/list name, the key, and the upated quantity to change
 # steps: update the quanityt number
 # output: updated list with the new quantity
 
+def update_quantity(list,item,item_count)
+	list[item] = item_count
+end
+
 # Method to print a list and make it look pretty
 # input: the list name
 # steps: iterate through the list with a puts statement and interpelation
 # output: a series of strings expressing what is contained in the list
+
+def print_list(list)
+	list.each do |item,count|
+		puts "#{item}: #{count}"
+	end
+end
+
+p add_item(grocery_list,"grape",4)
+p add_item(grocery_list,"carrot",5)
+p grocery_list
+# p delete_item(grocery_list,"grape")
+p grocery_list
+p update_quantity(grocery_list,"carrot",7)
+p grocery_list
+p print_list(grocery_list)
