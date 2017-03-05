6ee1095e41b894149d16b71fb3635132c27c7318
Brent Unkrich <bunkrich@gmail.com>
2017-03-05 18:57:23 -0500

    added new driver code from the assignment with the testing array called SANTAS

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,8 +26,18 @@ class Santa
 end
 
 #-----DRIVER CODE-----#
-bob = Santa.new("boy", "mixed")
-p bob.speak
-p bob.eat_milk_and_cookies("fatty")
-# p Santa.eat_milk_and_cookies("snickky")
-bob.testing
+# bob = Santa.new("boy", "mixed")
+# p bob.speak
+# p bob.eat_milk_and_cookies("fatty")
+# # p Santa.eat_milk_and_cookies("snickky")
+# bob.testing
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+p santas
