ba5e2b6b7e4351d4647e378545923ab7f577c067
Brent Unkrich <bunkrich@gmail.com>
2017-03-06 02:19:12 -0500

    finished the questions that i forgot to add on the bottom from my GPS

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -73,3 +73,25 @@ end
 transformed_list = transform_list(grocery_old_list)
 print_list(transformed_list)
 # puts transformed_list
+
+
+# ----- Release 4: Reflect -----
+# On your own, add a commented reflection section to your gps2_2.rb file. Answer the following questions in your reflection:
+
+# What did you learn about pseudocode from working on this challenge?
+# --- It's helpful to think ahead and plan out what you are going to be coding and problem solve first, then worry about syntax
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# --- Arrays can't support more than piece of info that conects to another where a hash can, which was needed for this.
+
+# What does a method return?
+# ---The last part of code that was executed, or whatever is after a "RETURN"
+
+# What kind of things can you pass into methods as arguments?
+# --- pretty much any object in Ruby: arrays, hashes, strings, integers, etc
+
+# How can you pass information between methods?
+# --- use a class level variable or have a variable hold the return value when it's called
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# --- Basic method understanding is good, I still need work on the overall syntax
