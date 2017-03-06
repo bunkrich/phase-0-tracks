ba3b8cde4056b36de828d41db3dde48f91a16c17
Brent Unkrich <bunkrich@gmail.com>
2017-03-06 11:30:08 -0500

    finished code

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -13,49 +13,38 @@ class WordGame
 		@guess_letters = []
 		@letter = letter
 	end
-	
 
-	def check_word(guess_word)
-	
-	end
-
-  def count_check
-    p @guess_count
-    if @guess_count == 0
-      puts "Sorry, you loose"
-      @is_over = true
-
-  end
-end
-
-	def check_letter(guess)
-		p wordssss = ""
-		p array = wordssss.split("")
+#checks on the number of counts, if you get to 0 you lose.
+	def count_check
+		puts @guess_word.join(" ")
+		if @guess_count == 0
+			puts "Sorry, you lose!"
+			@is_over = true
+		end
 	end
 
 #checks if there is a multiple letter
 	def guess_letter_multiple(letter1)
-	if @guess_letters.include? letter1
-    puts "you have already tried this letter..."
-	 else
-	  @guess_count -= 1
-	 	puts "remaining guesses: #{guess_count}"
-		@guess_letters << letter1
-	end
+		if @guess_letters.include? letter1
+			puts "You have already tried this letter..."
+		else
+			@guess_count -= 1
+			puts "Remaining guesses: #{guess_count}"
+			@guess_letters << letter1
+		end
 	end
 	 
 #checks if the letter is in the word then returns the array showing the blank spots and the right letter
 	def guess_letter_check(letter1)
-	  if @secret_word.include? letter1
-	  	p	spot_change = @secret_word.index(letter1)
-	  	p @guess_word
-	  		@guess_word.delete_at(spot_change)
-	  		p @guess_word.insert(spot_change,letter1)
-	  		puts "good job"
-	  		else 
-	  		 puts "sorry wrong letter..."
-	    end
-	 end
+		if @secret_word.include? letter1
+			spot_change = @secret_word.index(letter1)
+			@guess_word.delete_at(spot_change)
+			@guess_word.insert(spot_change,letter1)
+			puts "Good Job!"
+		else 
+			puts "Sorry wrong letter..."
+		end
+	end
 
 #checks if word is right
 	def guess_word_check
@@ -64,24 +53,24 @@ end
 		else 
 			@is_over = true
 			puts "YOU WON!"
-  end
-end
+		end
+	end
 end
 
+
+# ----- Driver Code -----#
+puts "Welcome to the Word Game!"
 #player one input
 secret_word_input = "unkrich"
-p winner_word = WordGame.new(secret_word_input)
+winner_word = WordGame.new(secret_word_input)
 
 #player two input
 while !winner_word.is_over
-	puts "guess your letter..."
+	puts "Guess a letter..."
 	guess_letter_input = gets.chomp
 	
-winner_word.guess_letter_multiple(guess_letter_input)
-winner_word.guess_letter_check(guess_letter_input)
-winner_word.guess_word_check
-winner_word.count_check
-
+	winner_word.guess_letter_multiple(guess_letter_input)
+	winner_word.guess_letter_check(guess_letter_input)
+	winner_word.guess_word_check
+	winner_word.count_check
 end
-
-
