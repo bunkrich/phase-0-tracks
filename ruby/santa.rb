d6d5fca2ccf9ada63e3c5160c052ab6e56612a68
Brent Unkrich <bunkrich@gmail.com>
2017-03-05 19:00:55 -0500

    tested out the alt driver code and it worked

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -32,12 +32,22 @@ end
 # # p Santa.eat_milk_and_cookies("snickky")
 # bob.testing
 
+# #-----DRIVER CODE: RELEASE 1-----#
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# p santas
+
+#-----DRIVER CODE: RELEASE 1. part 2-----#
 santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
 p santas
