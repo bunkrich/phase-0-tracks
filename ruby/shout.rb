170a626c86499629d2b85ecd4dc0fc4bde1be5e6
Brent Unkrich <bunkrich@gmail.com>
2017-03-06 01:41:31 -0500

    added 2 new classes to include the Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
+# module Shout
+# 	# we'll put some methods here soon, but this code is fine for now!
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "<3 >3" + " :) yaassss"
+# 	end
+# end
+
 module Shout
 	# we'll put some methods here soon, but this code is fine for now!
 	def self.yell_angrily(words)
@@ -8,3 +19,13 @@ module Shout
 		words + "<3 >3" + " :) yaassss"
 	end
 end
+
+class Fight
+	include Shout
+end
+
+class PartyTime
+	include
+end
+
+
