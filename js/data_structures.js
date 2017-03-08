564992eeb8d8a9adb5a16878fa556317f97fcd2c
Brent Unkrich <bunkrich@gmail.com>
2017-03-08 13:13:46 -0500

    added the arrays for color and names then added driver  code to add one color and one name.

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = ["blue", "green", "red", "brown", "orange"];
+var names = ["Misty", "Milly", "Manny", "Mama"];
+colors.push("pink");
+names.push("Molly");
+console.log(colors);
+console.log(names);
