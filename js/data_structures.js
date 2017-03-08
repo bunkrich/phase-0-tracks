9731158c1d97886ea1ce388203004b301514928c
Brent Unkrich <bunkrich@gmail.com>
2017-03-08 13:36:20 -0500

    added a variable to put the horse color with the horse name

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,17 @@
 var colors = ["blue", "green", "red", "brown", "orange"];
-var names = ["Misty", "Milly", "Manny", "Mama"];
+var names = ["Misty", "Milly", "Manny", "Mama", "Marge"];
 colors.push("pink");
 names.push("Molly");
-console.log(colors);
-console.log(names);
+// console.log(colors);
+// console.log(names);
+
+
+var horseinfo = {};
+
+
+for (var i = 0; i < colors.length; i++){
+	horsename = names[i];
+	horsecolor = colors[i];
+	horseinfo [horsename] = horsecolor;
+}
+	console.log(horseinfo);
