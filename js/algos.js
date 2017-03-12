81f8127b3c98bde8cc1358df7e68f61b2977f4b1
Brent Unkrich <bunkrich@gmail.com>
2017-03-12 18:23:26 -0400

    added the code for 2 function the compaires the input to see if it has been used already

--- js/algos.js
+++ js/algos.js
@@ -7,5 +7,37 @@ function findLongestWord(wordArray) {
 	return longestWord[0];
 }
 
-wordArray = ['brent','lee','unkrichfdfdfdfddf','not','gooosegoose'];
-findLongestWord(wordArray);
+// start with a blank array
+var values = [];
+// first function pulls in all the info and sends it to the compaire function
+function compaireBoth(key1, value1, key2, value2) {
+	  //console.log (values);
+	  compaire2(value1);
+	  compaire2(value2);
+}
+
+// takes in the value and finds the index, if it hasn't been used it will be -1 and will not do anything
+// it then adds the new value to the array	  
+function compaire2(value) {
+	   a = values.indexOf(value);
+    values.push(value);
+    if (a >= 0) {
+      console.log (value);
+      console.log ("It's a match!");
+    }
+}
+
+// ----- DRIVER CODE (Release 1) -----
+compaireBoth('name:', 'steven', 'age:', 7);
+compaireBoth('name:', 'steven', 'age:', 10);
+compaireBoth('name:', 'steven', 'age:', 7);
+compaireBoth('name:', 'Jenny', 'age:', 8);
+compaireBoth('name:', 'Greg', 'age:', 9);
+compaireBoth('name:', 'Jenny', 'age:', 10);
+
+
+// ----- DRIVER CODE (Release 0) -----
+//wordArray = ['brent','lee','unkrichfdfdfdfddf','not','gooosegoose'];
+//findLongestWord(wordArray);
+
+// function that will compaire if 2 objects have anything in common
