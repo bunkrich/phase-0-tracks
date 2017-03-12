2997d331ab083ad4f117220021bd6f544197274c
Brent Unkrich <bunkrich@gmail.com>
2017-03-12 14:56:49 -0400

    added my code and debugged, also added a true/if to print or not to print the resules

--- js/explore.js
+++ js/explore.js
@@ -2,3 +2,17 @@
 // create a function that loops through the word
 // as it loops it rebuilds the word from the back to the front
 // then the program should return the value
+function reverseword(userword) {
+	console.log("the original word was:")
+	console.log(userword)
+	var print = true; 
+	var newword = '';
+	for (var word = userword.length - 1; word >= 0; word--)
+		newword += userword[word];
+	 if (print) {console.log ("the new word is:");
+	 console.log(newword)}
+}
+
+// driver code
+var userword = 'brentunkrich';
+reverseword (userword);
