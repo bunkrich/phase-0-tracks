ad9f0186db5a027aede7bdff72fdb4162c7b3f5d
Brent Unkrich <bunkrich@gmail.com>
2017-03-12 14:09:53 -0400

    pseudocoded the program for the loop that will reverse a word

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// take a variable from user input
+// create a function that loops through the word
+// as it loops it rebuilds the word from the back to the front
+// then the program should return the value
